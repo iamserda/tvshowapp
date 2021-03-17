b9014a8057b17e7203663139cec034050ee2bac8
Serda Simus <iamserda@gmail.com>
2021-03-17 14:11:49 -0400

    search ability, improved look and feel and more. iamserda

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
-# TV Guide with TVMaze-API
+# title(){
+		
+		return this.options.title || this.options.name;
+	}
 
-### What is it?
-Learning to build user interfaces using CSS grid and a public API.
+Quick start:
 
-## Where is the Link?
-<a href="https://iamserda.github.io/TVMaze-API/" target="_blank">TVGuide</a>
+```
+$ yarn # npm install
+$ yarn build # npm run build
+````
 
+## Development
 
-Questions, concerns, emotional outbursts? Reach out!
-- Twitter: <a href="https://twitter.com/iamserda">@iamserda</a>
-- LinkedIn: <a href="https://linkedin.com/in/iamserda" target="_blank">iamserda</a>
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
 
-Made with ❤️ in Inwood, NYC. Circa March 2021.
-# TVGuide with TVMaze API
+```
+$ yarn watch # npm run watch
+```
+
+## Supporting Scrimba
+
+Since 2017, we have created over 20 free courses on Scrimba, and we're going to
+continue launching free courses. But to pay our bills, we have to charge once
+in a while. So if you've ever wanted to "give back" to Scrimba, you can do that by buying
+	one of our paid courses
+
+- [Become a professional React developer](https://scrimba.com/course/greact)
+- [The Responsive Web Design Bootcamp](https://scrimba.com/course/gresponsive)
+- [The Ultimate JavaScript Bootcamp](https://scrimba.com/course/gjavascript)
+
+	It would also mean the world to us if you share the courses.  
+
+Happy Coding!
--- index.js
+++ index.js
@@ -12,86 +12,116 @@
         - Seasons listed as individual divs
 */
 
+/* fetches an api: tvMaze, returns an object containing data points about a tvshow */
+/* tvshow: smackdown, name, pictures, summary of the show */
 const findShow = async (query) => {
-    const embed = "&embed=seasons";
-    const url = `https://api.tvmaze.com/singlesearch/shows?q=${ query }${ embed }`;
+    const embed = '&embed=seasons';
+    const url = `https://api.tvmaze.com/singlesearch/shows?q=${query}${embed}`;
+    const response = await fetch(url);
 
     try {
-        const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             return data;
         } else {
-            throw new Error(`Error: findShow(url) ${ response.status }`);
+            throw new Error(`Error: findShow(url) ${response.status}`);
         }
-    } catch (err) {
-        console.error(err);
-    }
-};
+    } catch (err) { console.error(err) }
 
+}
+
+/* creates the container that will host the app */
 const createApp = () => {
-    const app = document.createElement("div");
+    const app = document.createElement('div');
     app.id = "app";
-    app.classList.add("app");
+    app.classList.add('app');
 
     return app;
-};
+}
 
-const displayShow = async () => {
-    const data = await findShow("Smackdown");
-    console.log(data);
-    const {
-        name,
-        image: { medium },
-        summary,
-        _embedded: { seasons },
-    } = data;
-    console.log(data);
-    let html = `
-                <div class='showHeader title'>
-                    <img class="cover" href=${ medium } alt="someAltName: ${ name }" />
-                    <h3 class="show-title">${ name }</h3>
-                </div>
-                <div class="summary">${ summary }</div>
-                <div class="seasons"></div>
-             `;
-    document.getElementById("app").innerHTML = html;
+/*allowing the user to request a new show from the api.*/
+const displaySearchBar = () => {
+    const searchDiv = `
+        <div id="searchBar" class="search">
+            <input type="text" id="search-term" name="search-bar" placeholder="Enter Show Name" />
+            <button id="submit">Search</search>
+        </div>
+        `
+    return searchDiv;
+}
+
+const displayShow = async (query) => {
+    if (!query) {
+        query = 'Smackdown';
+    }
+
+    const data = await findShow(query);
+    //  console.log(data)     
+    const { name, image: { medium }, summary, _embedded: { seasons } } = data;
     console.log(medium);
+    let html = `
+        <img class="cover" src=${medium} alt="${name}" />
+        <h2 class="title">${name}</h2>
+        <div class="summary">${summary}</div>
+        <div id="seasons" class="seasons"></div>
+     `
+    document.getElementById('app').innerHTML = html;
+    // console.log(medium)
     return seasons;
+}
+
+const displaySeasons = async (data) => {
+    const seasonsDiv = document.getElementById('seasons');
+    data.forEach(
+        item => {
+            let { name, number, image } = item;
+            if (!name) {
+                name = document.getElementsByClassName('title').item(0).textContent;
+            }
+            if (!image) {
+                image = document.getElementsByClassName('cover').item(0).src;
+            }
+            const html = `
+                <div class="season">
+                    <img 
+                        class="season-cover" src=${image}
+                    alt=${name}/>
+                    <p class="season-title">${name}</p>
+                    <p class="season-number">Season: ${number}</p>
+                </div>
+           `
+            seasonsDiv.innerHTML += html;
+        })
 };
 
-// const displaySeasons = async (data) => {
-//     //   const seasonsDiv = document.getElementById("seasons");
-//     //   console.log(seasonsDiv);
-//     //   console.log(data[1]);
-//     //   data.forEach((item) => {
-//     //     const { name, number, image } = item;
-//     //     if (!name) {
-//     //       name = document.getElementById("title").innerText;
-//     //     }
-//     //     if (!image) {
-//     //       // let imgElement = document.getElementsByClassName('showHeader').item(0);
-//     //       // image = imgElement.href;
-//     //       image = "576808.jpg";
-//     //     }
-//     //     console.log(name, number, image);
-//     //     const html = `
-//     //             <div class="season">
-//     //                 <img class="cover" href=${image}/>
-//     //                 <p class"title">${name}</p>
-//     //                 <p class"title">Season: ${name}</p>
-//     //             </div>
-//     //            `;
-//     //     seasonsDiv.innerHTML += html;
-//     //   });
-// };
+(
+    async function () {
+        let query = "";
+        const body = document.body;
+        const app = createApp();
+        const h1 = document.createElement('h1');
+        const searchDiv = displaySearchBar();
+        
+        h1.classList.add('app-title');
+        h1.innerText = "CSS Grid: TVMaze API"
 
-(async function () {
-    const app = createApp();
-    document.getElementById('main').appendChild(app);
-    //   const main = document.getElementById("main");
-    //   const app = createApp();
-    //   main.prepend(app);
-    //   const ss = await displayShow();
-    //   displaySeasons(ss);
-})();
+        body.prepend(app)
+        body.prepend(h1) // positions h1 as 1st element.
+        h1.insertAdjacentHTML('afterend', searchDiv);
+
+        const ss = await displayShow()
+        displaySeasons(ss)
+
+        const submitButton = document.getElementById('submit');
+        submitButton.onclick = async () => {
+            const seasonsDiv = document.getElementById('seasons');
+            seasonsDiv.innerHTML = "";
+            query = document.getElementById('search-term').value;
+            console.log(query);
+            const ss = await displayShow(query)
+            displaySeasons(ss)
+        }
+        // query
+    }
+
+)();
